82bf193b9f8ec6e7989f9df7de85249265efd717
shubhammane8668 <shubhsmane@gmail.com>
2022-07-04 00:39:36 +0530

    Comand line checked

---
+++ fifth_prblm.java
@@ -0,0 +1,10 @@
+
+public class fifth_prblm {
+
+	public static void main(String[] args) {
+		//  Sum of command-line arguments and count the invalid integers entered.
+
+		System.out.println("Your first argument is: "+args[2]);
+}
+
+}
