04388584a76fb8107025c2a02f3607344f36e8fc
shubhammane8668 <shubhsmane@gmail.com>
2022-07-04 00:33:41 +0530

    string checked

---
+++ third_prblm.java
@@ -0,0 +1,17 @@
+
+public class third_prblm{
+
+	public static void main(String[] args) {
+		// program to check two strings are equal or not.
+		
+         String s1="Sachin";  
+	      String s2="Sachin";   
+		    String s3="Saurav";  
+		
+		    System.out.println(s1.equals(s2));  
+		   System.out.println(s1.equals(s3));  
+ 
+
+	}
+
+}
