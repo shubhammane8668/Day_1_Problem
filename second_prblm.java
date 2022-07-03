364de8229eb4017b53296812e4f0ad6eab6cdd16
shubhammane8668 <shubhsmane@gmail.com>
2022-07-04 00:24:15 +0530

    primitive data type

---
+++ second_prblm.java
@@ -0,0 +1,19 @@
+
+public class second_prblm {
+
+	public static void main(String[] args) {
+		// Program to display the default value of all primitive data types of Java
+ 
+		int a=1;
+		double b=2.01; 
+		char c='c';
+		boolean d=false;
+	
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	
+	}
+	
+}
